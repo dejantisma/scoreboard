aff35757c068ea4353ff28c576b93211f339ab3c
dejantisma <dejan.tisma@maine.edu>
2021-11-16 11:35:20 -0500

    data fetching & working scoreboard

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    images: {
+      domains: ['assets.acme.com'],
+    },
+  }
+
+  /**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    images:{
+        domains:['cdn.nba.com'],
+    },
+  }
+  
+  module.exports = nextConfig
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,28 +1,63 @@
 import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import { getAllGameIds, getGameData } from '../../lib/posts'
+import Image from 'next/image'
 import Head from 'next/head'
-import Date from '../../components/date'
+import styles from '../../components/layout.module.css'
+import Link from 'next/link'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Nav, Navbar, Alert } from 'react-bootstrap';
 import utilStyles from '../../styles/utils.module.css'
 
-export default function Post({ postData }) {
+export default function Post({ gameData }) {
+  var game = gameData.result[0];
+  var homeTeamLogoURL = `https://cdn.nba.com/logos/nba/${game.homeTeam.teamId}/primary/L/logo.svg`;
+  var awayTeamLogoURL = `https://cdn.nba.com/logos/nba/${game.awayTeam.teamId}/primary/L/logo.svg`;
+
   return (
+
+
     <Layout>
+
+
+
+
+
       <Head>
-        <title>{postData.title}</title>
+        <title>{game.gameCode}</title>
       </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
+
+      <div style={{ textAlign: 'center', }} >
+        <Image src={awayTeamLogoURL} height={200} width={200} className={utilStyles.imageInline}></Image>
+        <h1 className={utilStyles.scoreInline}>{game.awayTeam.score}</h1>
+        <h1 className={utilStyles.clockInline}> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{game.gameStatusText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
+        <h1 className={utilStyles.scoreInline}>{game.homeTeam.score}  </h1>
+        <Image src={homeTeamLogoURL} height={200} width={200} className={utilStyles.imageInline}></Image>
+
+      </div>
+
+
+      
+
+
+
+
+
+      <h1 >  </h1>
+
+
     </Layout>
+
+
+
+
+
   )
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = await getAllGameIds();
+  console.log('hi');
+  console.log(paths);
   return {
     paths,
     fallback: false
@@ -30,10 +65,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  const gameData = await getGameData(params.id)
   return {
     props: {
-      postData
+      gameData
     }
   }
 }
